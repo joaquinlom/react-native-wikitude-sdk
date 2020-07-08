1141be92fee1ce08e6e65d1097f170bd02ed1f7d
joaquin <joaquin.lom@spinar.mx>
2020-07-08 14:04:54 -0600

    trying to fix nullpoing

--- ios/WikitudeSdk.m
+++ ios/WikitudeSdk.m
@@ -69,7 +69,7 @@ RCT_EXPORT_MODULE()
     [self checkCameraPermissions:^(BOOL granted) {
            self.hasCameraPermission = &(granted);
     }];
-    if(_wikitudeView != NULL){
+    if(_wikitudeView != nil){
         return _wikitudeView;
     }
    _wikitudeView = [WikitudeView new];
