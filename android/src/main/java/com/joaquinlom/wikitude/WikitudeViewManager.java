d41c74a0a6cbd7b280b6e421b73b39041ed76266
joaquin <joaquin.lom@spinar.mx>
2020-07-11 13:06:33 -0600

    add screenshot on android

--- android/src/main/java/com/joaquinlom/wikitude/WikitudeViewManager.java
+++ android/src/main/java/com/joaquinlom/wikitude/WikitudeViewManager.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.Bitmap;
 import android.os.Handler;
 import android.os.Looper;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.util.Log;
+import android.util.Base64;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsoluteLayout;
@@ -20,6 +22,7 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import java.net.URL;
+import java.io.ByteArrayOutputStream;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.BaseActivityEventListener;
@@ -47,7 +50,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
-public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implements ArchitectJavaScriptInterfaceListener, ArchitectView.ArchitectWorldLoadedListener {
+public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implements ArchitectJavaScriptInterfaceListener, ArchitectView.ArchitectWorldLoadedListener, ArchitectView.CaptureScreenCallback {
 
 
     //Commands
@@ -56,6 +59,7 @@ public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implemen
     public static final int COMMAND_INJECT_LOCATION = 3;
     public static final int COMMAND_STOP_AR = 4;
     public static final int COMMAND_RESUME_AR = 5;
+    public static final int COMMAND_CAPTURE_SCREEN = 6;
     //public static final int COMMAND_GET_ANNOTATIONS = 4;
 
     WikitudeView wikitude;
@@ -157,7 +161,9 @@ public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implemen
                 "stopARMode",
                 COMMAND_STOP_AR,
                 "resumeARMode",
-                COMMAND_RESUME_AR);
+                COMMAND_RESUME_AR,
+                "captureScreen",
+                COMMAND_CAPTURE_SCREEN);
         return commandMap;
     }
     public WikitudeView getWikitudeView(){
@@ -231,6 +237,7 @@ public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implemen
         }
     }
     
+
     @ReactProp(name = "feature")
     public void setFeature(WikitudeView view, int feature){
 
@@ -324,11 +331,35 @@ public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implemen
                         MapBuilder.of("bubbled", "onFinishLoading"))).
                  put(
                      "onFailLoading",
-                         MapBuilder.of(
-                                 "phasedRegistrationNames",
-                         MapBuilder.of("bubbled", "onFailLoading")))
+                         MapBuilder.of( "phasedRegistrationNames",MapBuilder.of("bubbled", "onFailLoading")))
+                .put(
+                    "onScreenCaptured",
+                    MapBuilder.of("phasedRegistrationNames",MapBuilder.of("bubbled","onScreenCaptured"))
+                )
                 .build();
     }
+
+    @Override
+    public void onScreenCaptured(Bitmap image){
+        WritableMap event = Arguments.createMap();
+
+        //Bitmap to byte[]
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();  
+        image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        
+        event.putString("image",encoded);
+        ReactContext reactContext = this.ctx;
+        Log.d(TAG,"Screenshot capture");
+        //Log.d(TAG,"image "+encoded);
+
+        reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                this.wikitude.getId(),
+                "onScreenCaptured",
+                event);        
+    }
+
     @Override
     public void onJSONObjectReceived(JSONObject jsonObject) {
         Log.d("Wikitude onJsonReceived","jsonObject receive");
@@ -409,6 +440,9 @@ public class WikitudeViewManager extends ViewGroupManager<WikitudeView> implemen
                 //root.onPause();
                 stopAR();
                 break;
+            case COMMAND_CAPTURE_SCREEN:
+                root.captureScreen(args.getBoolean(0));
+                break;
         }
     }
 }
@@ -471,7 +505,16 @@ class WikitudeView extends ArchitectView{
         startUpConfig.setLicenseKey( license );
         createWikitude();
     }
-
+    public void captureScreen(Boolean mode){
+        int insideMode;
+        if(mode){
+            insideMode = ArchitectView.CaptureScreenCallback.CAPTURE_MODE_CAM_AND_WEBVIEW;
+        }else{
+            insideMode = ArchitectView.CaptureScreenCallback.CAPTURE_MODE_CAM;
+        }
+        Log.d(TAG,"CaptureScreen called, MODE: "+ String.valueOf(insideMode));
+        this.captureScreen(insideMode,ctxManager);
+    }
     public void loadWorld(){
         try{
             Log.d(TAG,this.url);
